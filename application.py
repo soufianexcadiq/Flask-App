c2839e9a20505fbffecb43cfdc31d3e48aed83ef
soufiane cadiq <cadiqsoufiane20@gmail.com>
2025-11-16 14:37:09 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 application = Flask(__name__)
@@ -7,4 +8,5 @@ def home():
     return render_template("index.html", message="CI/CD Flask App Working!")
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    application.run(host="0.0.0.0", port=port)
