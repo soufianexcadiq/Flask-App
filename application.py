19b344015e4ef9dfcc97431b52638a93b3e60a9a
soufiane cadiq <cadiqsoufiane20@gmail.com>
2025-11-16 14:53:41 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, render_template
 
-application = Flask(__name__)
+application = Flask(__name__)  # Must be 'application'
 
 @application.route("/")
 def home():
-    return render_template("index.html", message="CI/CD Flask App Working!")
+    return render_template("index.html", message="Flask CI/CD Works!")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
